a1ce9ece2d3c059e5aafd61fe1efb2eb0b61c682
Juan Ramirez <avengedforromance@hotmail.com>
2018-09-26 10:31:45 -0500

    Full working app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Flashlight } from "@ionic-native/flashlight";
+import { Platform } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isOn: boolean;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private platform:Platform, private flashlight: Flashlight) {
+    this.platform.ready().then(()=>{
+      this.updateFlashlightStatus();
+    });
+  }
 
+  switchOn(){
+    this.flashlight.switchOn();
+  }
+
+  switchOff(){
+    this.flashlight.switchOff();
+    this.updateFlashlightStatus();
+  }
+
+  toggle(){
+    this.flashlight.toggle();
+    this.updateFlashlightStatus();
+  }
+
+  updateFlashlightStatus(){
+    this.isOn = this.flashlight.isSwitchedOn();
   }
 
 }
